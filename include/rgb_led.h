5edc59bf7afaa33614880683945ca227f43087ee
Karl Oskar Lember <oskar.lember@gmail.com>
2018-09-06 15:58:19 +0300

    Added RGB led class

---
+++ include/rgb_led.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "led.h"
+#include <mbed.h>
+
+class RGBLED {
+  LED red_, green_, blue_;
+
+  struct Color {
+    bool red, green, blue;
+  };
+
+public:
+  static constexpr Color BLACK = Color{0, 0, 0};
+  static constexpr Color RED = Color{1, 0, 0};
+  static constexpr Color GREEN = Color{0, 1, 0};
+  static constexpr Color BLUE = Color{0, 0, 1};
+  static constexpr Color YELLOW = Color{1, 1, 0};
+  static constexpr Color PURPLE = Color{1, 0, 1};
+  static constexpr Color CYAN = Color{0, 1, 1};
+  static constexpr Color WHITE = Color{1, 1, 1};
+
+  RGBLED(PinName red, PinName green, PinName blue, bool active_low = false)
+      : red_(red, active_low), blue_(blue, active_low), green_(green, active_low) {}
+  inline void set(Color color) {
+    red_ = color.red;
+    blue_ = color.blue;
+    green_ = color.green;
+  };
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-#include "led.h"
+#include "rgb_led.h"
 #include <mbed.h>
 
 int main() {
-  LED test_led(PC_6, true);
+  RGBLED status_led(PC_6, PC_7, PC_8, true);
 
   while (1) {
-    test_led.on();
+    status_led.set(RGBLED::WHITE);
     wait_ms(100);
-    test_led.off();
+    status_led.set(RGBLED::BLACK);
     wait_ms(900);
   }
 }
