783ba3fc66deef17119aec3bc057136cbd6c7522
Karl Oskar Lember <oskar.lember@gmail.com>
2018-09-06 13:33:14 +0300

    Initial blink code test

--- include/led.h
+++ include/led.h
@@ -4,8 +4,11 @@
 
 class LED {
   DigitalOut led_pin_;
+  bool active_low_;
 
 public:
-  LED(PinName pin_id) : led_pin_(pin_id){};
-  void Invert() { led_pin_ = !led_pin_; }
+  LED(PinName pin_id, bool active_low = false) : led_pin_(pin_id), active_low_(active_low){};
+  inline void invert() { led_pin_ = !led_pin_; }
+  inline void on() { led_pin_ = true ^ active_low_; }
+  inline void off() { led_pin_ = false ^ active_low_; }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 #include <mbed.h>
 
 int main() {
-  LED test_led(PA_5);
+  LED test_led(PC_6, true);
 
   // put your setup code here, to run once:
 
+  test_led.off();
+
   while (1) {
-    test_led.Invert();
+    test_led.on();
     wait_ms(100);
+    test_led.off();
+    wait_ms(900);
   }
 }
