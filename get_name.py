5b7a61e902c71c41b53e0a0c5163663f14602cb2
Karl Oskar Lember <oskar.lember@gmail.com>
2018-09-06 14:50:36 +0300

    Added firmware version number to filename

---
+++ get_name.py
@@ -0,0 +1,12 @@
+Import("env")
+
+import subprocess
+
+# my_flags = env.ParseFlags(env['BUILD_FLAGS'])
+# defines = {k: v for (k, v) in my_flags.get("CPPDEFINES")}
+# print defines
+
+description = subprocess.Popen(
+    ["git", "describe", "--always"], stdout=subprocess.PIPE).stdout.read().strip()
+
+env.Replace(PROGNAME="firmware_%s" % description)
