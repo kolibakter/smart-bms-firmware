006524dd4d7dade7432286a86e900801c97817c6
Karl Oskar Lember <oskar.lember@gmail.com>
2018-09-10 18:49:39 +0300

    Added rgb_pwm_led class

---
+++ lib/rgb_pwm_led/rgb_pwm_led.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <pwm_led.h>
+#include <mbed.h>
+
+class RGBPWMLED {
+  PWMLED red_, green_, blue_;
+
+  struct Color {
+    float red, green, blue;
+  };
+
+public:
+  static constexpr Color BLACK = Color{0.0f, 0.0f, 0.0f};
+  static constexpr Color RED = Color{1.0f, 0.0f, 0.0f};
+  static constexpr Color GREEN = Color{0.0f, 1.0f, 0.0f};
+  static constexpr Color BLUE = Color{0.0f, 0.0f, 1.0f};
+  static constexpr Color YELLOW = Color{1.0f, 1.0f, 0.0f};
+  static constexpr Color PURPLE = Color{1.0f, 0.0f, 1.0f};
+  static constexpr Color CYAN = Color{0.0f, 1.0f, 1.0f};
+  static constexpr Color WHITE = Color{1.0f, 1.0f, 1.0f};
+
+  RGBPWMLED(const PinName red, const PinName green, const PinName blue, const bool active_low = false)
+      : red_(red, active_low), green_(green, active_low), blue_(blue, active_low) {}
+  inline void set(const Color color) {
+    red_ = color.red;
+    blue_ = color.blue;
+    green_ = color.green;
+  };
+    RGBPWMLED &operator=(const Color value) {
+    set(value);
+    return *this;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <mbed.h>
-#include <pwm_led.h>
+#include <rgb_pwm_led.h>
 
 int main() {
-  PWMLED status_led(PC_6, true);
+  RGBPWMLED status_led(PC_6, PC_7, PC_8, true);
 
   while (1) {
-    status_led = 0.8f;
-    wait_ms(100);
-    status_led = 0.1f;
-    wait_ms(900);
+    status_led = {0.8f, 0.1f, 0.1f};
+    wait_ms(333);
+    status_led = {0.1f, 0.8f, 0.1f};
+    wait_ms(333);
+    status_led = {0.1f, 0.1f, 0.8f};
+    wait_ms(333);
   }
 }
