c9f0437b05b001b17c5ed6bac287e3dd8535d3be
Karl Oskar Lember <oskar.lember@gmail.com>
2018-09-13 16:46:28 +0300

    Tested rosserial and seems to work

--- lib/smartbms/smartbms.cpp
+++ lib/smartbms/smartbms.cpp
@@ -1,12 +1,17 @@
 #include "smartbms.h"
 
-SmartBMS::SmartBMS() : ok_(true), status_led_({PC_6, PC_7, PC_8, 1.0f, true}), comm_(PA_2, PA_3) {
+SmartBMS::SmartBMS()
+    : ok_(true), status_led_({PC_6, PC_7, PC_8, 1.0f, true}), pub_int_("anint", &int_) {
   status_led_ << StatusLED::RAINBOW;
+  nh_.initNode();
+  nh_.advertise(pub_int_);
 }
 
 void SmartBMS::run() {
   while (ok_) {
-    comm_.write("sup?");
+    int_.data++;
+    pub_int_.publish(&int_);
+    nh_.spinOnce();
     wait(1);
   }
 }
--- lib/smartbms/smartbms.h
+++ lib/smartbms/smartbms.h
@@ -1,7 +1,14 @@
 #pragma once
 
 #include <comm.h>
+#include <ros.h>
 #include <status_led.h>
+#include <std_msgs/Int32.h>
+
+class BMSHardware : public MbedHardware {
+public:
+  BMSHardware() : MbedHardware(PA_2, PA_3){};
+};
 
 class SmartBMS {
 public:
@@ -11,5 +18,9 @@ public:
 private:
   bool ok_;
   StatusLED status_led_;
-  Comm comm_;
+
+  // ROS things
+  ros::NodeHandle_<BMSHardware> nh_;
+  std_msgs::Int32 int_;
+  ros::Publisher pub_int_;
 };
